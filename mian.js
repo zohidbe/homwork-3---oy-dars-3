06cee16e3a15add4acfebcc0f6f91612d007ba00
zohidbe <zohaxaydarov@gmail.com>
2024-08-04 21:56:35 +0500

    end hom workd

---
+++ mian.js
@@ -0,0 +1,54 @@
+// task 1
+
+// let son = 7;
+
+// if (son > 5) {
+//     son += 1;
+// }
+// console.log(son);
+
+// task 2
+
+// let son = 9;
+
+// if (son > 0) {
+//     son += 1;
+// } else  {
+//     son -= 2;
+// }
+// console.log(son);
+
+
+// task 3 
+
+// let son = 3;
+
+// if (son > 0) {
+//     son += 1;
+// } else if (son < 0) {
+//     son -= 2;
+// } else {
+//     son = 10;
+// }
+// console.log(son);
+
+
+// task 4
+
+// let son1 = 5, son2 = 10;
+
+// if (son1 > son2) {
+//     console.log(son1);
+// } else {
+//     console.log(soon2);
+// }
+
+// task 5
+
+// let son1 = 5, son2 = 10;
+
+// if (son1 < son2) {
+//     console.log("birinchi kichik son " + son1);
+// } else {
+//     console.log("ikinchi son kichik " + son2);   
+// }
